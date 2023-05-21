fdfafcb302c1a9aa68a826e0b7a512b6c1b503c6
santova <silviaadantova@gmail.com>
2023-05-21 15:46:14 +0300

    fix file

--- taino_dnevniche.c
+++ taino_dnevniche.c
@@ -1,24 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
-#include<windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-int line_count(FILE *ptr)
+int line_count(FILE* ptr)
 {
-    int line_count = 0;
+    int curr_line = 1;
     char ch;
-    while((ch = fgetc(ptr)) != EOF)
+    do
     {
-        if(ch == '\n')
-        {
-            line_count++;
-        }
+        ch = fgetc(ptr);
+        if (ch == '\n')
+            curr_line++;
     }
-    return line_count;
+    while (ch != EOF);
+    fseek(ptr, 0, SEEK_SET);
+    return curr_line;
 }
 
-int character_count(FILE *ptr)
+int character_count(FILE* ptr)
 {
     int characters = 0;
     char ch;
@@ -26,67 +26,73 @@ int character_count(FILE *ptr)
     {
         characters++;
     }
-    return characters;    
+    fseek(ptr, 0, SEEK_SET); 
+    return characters;
 }
 
-char* encrypt(char* key, char* filename){
-    FILE *ptr;
-    ptr = fopen(filename,"r");
-    int characters = character_count(ptr);
-    char* new_str = malloc(sizeof(char) * (characters + 1));
-	new_str[characters] = '\0';
-    rewind(ptr);
+void encrypt(char* key, char* filename)
+{
+    FILE* ptr;
+    ptr = fopen(filename, "r");
     int lineCount = line_count(ptr);
+    int characters = character_count(ptr);
     int k = 0;
-    for(int i = 0; i < lineCount; i++)
+    for (int i = 1; i < lineCount; ++i)
     {
-        fgets(new_str, sizeof(characters + 1), ptr);
-        for (int j = strlen(key) - 1; i < strlen(new_str); k++, j--)
+        char* new_str = malloc(sizeof(char) * (characters + 1));
+        new_str[characters] = '\0';
+        printf("i: %d\n", i);
+        fgets(new_str, characters + 1, ptr);
+        printf("new_str: %s\n", new_str);
+        for (int j = strlen(key) - 1; j < strlen(new_str); k++, j--)
         {
-            if(j < 0)
+            if (j < 0)
             {
                 j = strlen(key) - 1;
             }
             new_str[k] += key[j];
         }
+        fclose(ptr);
+        printf("new_str - encrypt: %s\n", new_str);
+        ptr = fopen(filename, "a");
+        fprintf(ptr, "%s", new_str);
+        fclose(ptr);
     }
-    fclose(ptr);
-    ptr = fopen(filename,"a");
-    fprintf(ptr, "%s", new_str);
-    fclose(ptr);
-    return new_str;
+
+    //return new_str;
 }
 
-char* decrypt(char* encrypted_str, char* key) {
+char* decrypt(char* encrypted_str, char* key)
+{
     char* decrypted_str = malloc(sizeof(char) * (strlen(encrypted_str) + 1));
     strcpy(decrypted_str, encrypted_str);
     decrypted_str[strlen(encrypted_str)] = '\0';
+
     int i = 0;
     int j_rows = 0;
-    for (int j = strlen(key) - 1; i < strlen(encrypted_str); i++, j--) {
-        if (j < 0) {
+    for (int j = strlen(key) - 1; i < strlen(encrypted_str); i++, j--)
+    {
+        if (j < 0)
+        {
             j = strlen(key) - 1;
         }
         decrypted_str[i] -= key[j];
-        //decrypted_str[i] -= 1;
     }
     return decrypted_str;
 }
 
-
 int main()
 {
     char dnevnik[10000];
-    char password[20] = "1234";
-    //fgets(dnevnik, 10000, stdin);
+    char password[20] = "sisoni_123";
     char filename[] = "example.txt";
-    char command[50];
-    sprintf(command, "notepad.exe %s", filename);
-    system(command);
-    char* encrypted  = encrypt(password, filename);
-    printf("encrypted <%s>\n", encrypted);
+    encrypt(password, filename);
+    //printf("encrypted <%s>\n", encrypted);
+
     //char* decrypted = decrypt(encrypted, password);
-    //printf("encrypted <%s>, decrypted <%s>\n", encrypted, decrypted);
-    
+    //printf("decrypted <%s>\n", decrypted);
+
+    //free(decrypted);
+
     return 0;
 }
