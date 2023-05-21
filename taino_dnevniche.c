4906596bfaa42e0f7c179da8f8aefb009eb1445d
santova <silviaadantova@gmail.com>
2023-05-21 13:08:31 +0300

    add files

--- taino_dnevniche.c
+++ taino_dnevniche.c
@@ -2,8 +2,11 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdbool.h>
+#include <windows.h>
 
-char* encrypt(char* source, char* key){
+char* encrypt(char* source, char* key, char* filename){
+    FILE *ptr;
+    ptr = fopen(filename,"r");
     char* new_str = malloc(sizeof(char) * (strlen(source) + 1));
 	strcpy(new_str, source);
 	new_str[strlen(source)] = '\0';
@@ -15,7 +18,7 @@ char* encrypt(char* source, char* key){
         {
             j = strlen(key) - 1;
         }
-        new_str[i] = new_str[i] + key[j] + 1;
+        new_str[i] += key[j];// + key[j] + 1;
         
         /*if(new_str[i] == '\n')
         {
@@ -38,8 +41,8 @@ char* decrypt(char* encrypted_str, char* key) {
         if (j < 0) {
             j = strlen(key) - 1;
         }
-        decrypted_str[i] += key[j];
-        decrypted_str[i] -= 1;
+        decrypted_str[i] -= key[j];
+        //decrypted_str[i] -= 1;
     }
     return decrypted_str;
 }
@@ -48,9 +51,13 @@ char* decrypt(char* encrypted_str, char* key) {
 int main()
 {
     char dnevnik[10000];
-    char password[20] = "obicham_kotki";
+    char password[20] = "1234";
     fgets(dnevnik, 10000, stdin);
-    char* encrypted  = encrypt(dnevnik, password);
+    char filename[] = "example.txt";
+    char command[50];
+    sprintf(command, "notepad.exe %s", filename);
+    system(command);
+    char* encrypted  = encrypt(dnevnik, password, filename);
     printf("origunal <%s>, encrypted <%s>\n", dnevnik, encrypted);
     char* decrypted = decrypt(encrypted, password);
     printf("encrypted <%s>, decrypted <%s>\n", encrypted, decrypted);
