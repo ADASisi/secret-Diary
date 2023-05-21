229434d6c08a65d789e171f20efdf2b4c8b657a7
santova <silviaadantova@gmail.com>
2023-05-21 13:46:22 +0300

    encrypt func file

--- taino_dnevniche.c
+++ taino_dnevniche.c
@@ -2,32 +2,58 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdbool.h>
-#include <windows.h>
+#include<windows.h>
 
-char* encrypt(char* source, char* key, char* filename){
+int line_count(FILE *ptr)
+{
+    int line_count = 0;
+    char ch;
+    while((ch = fgetc(ptr)) != EOF)
+    {
+        if(ch == '\n')
+        {
+            line_count++;
+        }
+    }
+    return line_count;
+}
+
+int character_count(FILE *ptr)
+{
+    int characters = 0;
+    char ch;
+    while ((ch = fgetc(ptr)) != EOF)
+    {
+        characters++;
+    }
+    return characters;    
+}
+
+char* encrypt(char* key, char* filename){
     FILE *ptr;
     ptr = fopen(filename,"r");
-    char* new_str = malloc(sizeof(char) * (strlen(source) + 1));
-	strcpy(new_str, source);
-	new_str[strlen(source)] = '\0';
-    int i = 0;
-    int j_rows = 0;
-    for(int j = strlen(key) - 1; i < strlen(source); i++, j--)
+    int characters = character_count(ptr);
+    char* new_str = malloc(sizeof(char) * (characters + 1));
+	new_str[characters] = '\0';
+    rewind(ptr);
+    int lineCount = line_count(ptr);
+    int k = 0;
+    for(int i = 0; i < lineCount; i++)
     {
-        if(j < 0)
+        fgets(new_str, sizeof(characters + 1), ptr);
+        for (int j = strlen(key) - 1; i < strlen(new_str); k++, j--)
         {
-            j = strlen(key) - 1;
-        }
-        new_str[i] += key[j];// + key[j] + 1;
-        
-        /*if(new_str[i] == '\n')
-        {
-            if(key[j_rows] % 2 != 0)
+            if(j < 0)
             {
-
+                j = strlen(key) - 1;
             }
-        }*/
+            new_str[k] += key[j];
+        }
     }
+    fclose(ptr);
+    ptr = fopen(filename,"a");
+    fprintf(ptr, "%s", new_str);
+    fclose(ptr);
     return new_str;
 }
 
@@ -52,15 +78,15 @@ int main()
 {
     char dnevnik[10000];
     char password[20] = "1234";
-    fgets(dnevnik, 10000, stdin);
+    //fgets(dnevnik, 10000, stdin);
     char filename[] = "example.txt";
     char command[50];
     sprintf(command, "notepad.exe %s", filename);
     system(command);
-    char* encrypted  = encrypt(dnevnik, password, filename);
-    printf("origunal <%s>, encrypted <%s>\n", dnevnik, encrypted);
-    char* decrypted = decrypt(encrypted, password);
-    printf("encrypted <%s>, decrypted <%s>\n", encrypted, decrypted);
+    char* encrypted  = encrypt(password, filename);
+    printf("encrypted <%s>\n", encrypted);
+    //char* decrypted = decrypt(encrypted, password);
+    //printf("encrypted <%s>, decrypted <%s>\n", encrypted, decrypted);
     
     return 0;
 }
