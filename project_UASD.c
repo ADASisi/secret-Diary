70cbc53d288f33f2cb1768532c724807cc30b49b
Gotvacha <101015518+Gotvacha@users.noreply.github.com>
2023-06-10 00:05:58 +0300

    finalised project
    it uses identifiers

--- project_UASD.c
+++ project_UASD.c
@@ -1,30 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "search.c"
-#include "file_encryption.h"
+#include<time.h>
 
-#define NUMBER_OF_SYMBOLS 100//max number of symbols stored in a file
-#define SYMBOLS_IN_MENU 50
-#define HEADING_SUMBOLS 30
+#define MAX_FILE_LINE_LENGTH 100//max number of symbols stored in a file
+#define MAX_MENU_LINE_LENGTH 100
+#define HEADING_SYMBOLS 30
+#define MAX_FILENAME_LENGTH 50
 
 struct node{
-    char* name;
+    char* name,* filename;
     int day, month, year;
     struct node* next;
 };
 
-struct node* _init_(){
+struct node* _init_(){// NE PIPAJ
     struct node* node = malloc(sizeof(struct node));
     node->next = NULL;
     node->name = NULL;
+    node->filename = NULL;
     node->day = 0;
     node->month = 0;
     node->year = 0;
     return node;
 }
 
-void get_data(char* text, int* day, int* month, int* year){
+void get_data(char* text, int* day, int* month, int* year){// NE PIPAJ
     int i = 0, add = 0;
     while(text[i] != '@'){
         i++;
@@ -49,14 +50,14 @@ void get_data(char* text, int* day, int* month, int* year){
     i++;
     add = 0;
 
-    while (text[i] != '\n'){
+    while (text[i] != ' '){
         add = add * 10 + (text[i] - '0');
         i++;
     }
     *day = add;
 }
 
-void extract_name(char* text, char* name){
+void extract_name(char* text, char* name){// NE PIPAJ
     int i = 0;
     while(text[i] != '@'){
         name[i] = text[i];
@@ -65,7 +66,32 @@ void extract_name(char* text, char* name){
     name[i-1] = '\0';
 }
 
-void make_uppercase(char* heading){  //to upper
+void extract_filename(char* text, char* filename){// NE PIPAJ
+    int i = 0, j = 0;
+    char* c;
+    while(text[i] != '@'){
+        i++;
+    }
+    i++;
+    while(text[i] != '@'){
+        i++;
+    }
+    i += 2;
+    c = text + i;
+    strcpy(filename, c);
+    filename[strlen(filename) - 1] = '\0';
+    /*
+    while(text[i] != '\0'){
+        filename[j] = text[i];
+        printf("%c", filename[j]);
+        j++;
+    }
+    filename[j] = '\0';
+    */
+}
+
+
+void make_uppercase(char* heading){// NE PIPAJ
     int i = 0;
     while(heading[i] != '\0'){//\n when using gets
         if(heading[i] > 96 && heading[i] < 123){
@@ -75,23 +101,66 @@ void make_uppercase(char* heading){  //to upper
     }
 }
 
-void insert_node(struct node** head, char* name, int day, int month, int year){
+void generate_filename(char* filename){// NE PIPAJ
+    int i = time(NULL), j = 0;
+    while(j < MAX_FILENAME_LENGTH - 6){
+        i = (((j+i)*i)%26) + 97;
+        filename[j] = i;
+        j++;
+    }
+    filename[j+1] = '\0';
+    strcat(filename, ".txt");
+}
+
+/*
+void generate_filename(char* name, int day, int month, int year, char* filename){// NE PIPAJ
+    int i = 0, j = 0;
+
+    while(j < MAX_FILENAME_LENGTH - 6){
+        if(name[i] != '\0'){
+            filename[i] = name[i];
+            i++;
+        }
+        else{
+            filename[j] = (((j+i)*i)%26) + 97;
+        }
+        if(j == day){
+            filename[j] = day + 64;
+        }
+        if(j == day + month){
+            filename[j] = month + 47;
+        }
+        j++;
+    }
+    filename[j] = (year % 26) + 97;
+    filename[j+1] = '\0';
+    strcat(filename, ".txt");
+}
+*/
+
+void insert_node(struct node** head, char* name, char* filename, int day, int month, int year) {// NE PIPAJ
     struct node* new_node = _init_();
-    int i = 0;
-    new_node->name = malloc(strlen(name)*sizeof(char) + 1);
+    new_node->name = malloc((strlen(name) + 1) * sizeof(char));
+    new_node->filename = malloc((strlen(filename) + 1) * sizeof(char));
     strcpy(new_node->name, name);
+    strcpy(new_node->filename, filename);
     new_node->day = day;
     new_node->month = month;
     new_node->year = year;
     make_uppercase(new_node->name);
 
-     if ((*head) == NULL || (*head)->year > year || ((*head)->year == year && (*head)->month > month) || ((*head)->year == year && (*head)->month == month && (*head)->day > day)){
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    }
+
+    if ((*head)->year > year || ((*head)->year == year && (*head)->month > month) || ((*head)->year == year && (*head)->month == month && (*head)->day > day)) {
         new_node->next = *head;
         *head = new_node;
     }
     else {
         struct node* temp = *head;
-        while (temp->next != NULL && (temp->next->year < year || (temp->next->year == year && temp->next->month < month) || (temp->next->year == year && temp->next->month == month && temp->next->day < day))){
+        while (temp->next != NULL && (temp->next->year < year || (temp->next->year == year && temp->next->month < month) || (temp->next->year == year && temp->next->month == month && temp->next->day < day))) {
             temp = temp->next;
         }
         new_node->next = temp->next;
@@ -99,14 +168,13 @@ void insert_node(struct node** head, char* name, int day, int month, int year){
     }
 }
 
-void getting_menu(struct node** head, char* text){
+void getting_menu(struct node** head, char* text){// NE PIPAJ
     int day, month, year;
-    char name[HEADING_SUMBOLS];
+    char name[HEADING_SYMBOLS], filename[MAX_FILENAME_LENGTH];
     get_data(text, &day, &month, &year);
-    printf("%d, %d, %d", day, month, year);
     extract_name(text, name);
-    printf("\n%s\n", name);
-    insert_node(head, name, day, month, year);
+    extract_filename(text, filename);
+    insert_node(head, name, filename, day, month, year);
 }
 
 void print_menu(struct node* head){
@@ -126,40 +194,12 @@ void print_menu(struct node* head){
     }
 }
 
-void fillingHashtables(struct node* head, struct hashtable* table_for_dates, struct hashtable* table_for_titles){
-    struct node* itt = head;
-    if(itt->name != NULL){
-        while(itt != NULL){
-            itt = itt->next;
-            //table_for_titles = hashtable_add(table_for_titles, itt->name, dateToString(itt->day, itt->month, itt->year), itt->id);
-            //table_for_dates = hashtable_add(table_for_dates, dateToString(itt->day, itt->month, itt->year), itt->name, itt->id);
-        }
-    }
-}
-
-int check_if_file_exists(char* name){
-    FILE* file;
-    if((file = fopen(name,"r")) == NULL){
-        fclose(file);
-        return 0;//doesn't exist
-    }
-    else{
-        printf("STORY WITH THE SAME NAME ALREADY EXIST\n");
-        fclose(file);
-        return 1;//exists
-    }
-}
-
 void add_story(struct node** head){
-    char name[HEADING_SUMBOLS], text[NUMBER_OF_SYMBOLS], filename[HEADING_SUMBOLS + 4];
-    int day, month, year, i = -1;
-    do{
-        printf("\nENTER NAME OF STORY: ");
-        getchar();
-        gets(name);
-        strcpy(filename, name);
-        strcat(filename, ".txt");
-    }while(check_if_file_exists(filename) == 1);
+    char name[HEADING_SYMBOLS], text[MAX_FILE_LINE_LENGTH], filename[MAX_FILENAME_LENGTH];
+    int day, month, year, i = 0;
+    printf("\nENTER NAME OF STORY: ");
+    getchar();
+    gets(name);
     do{
         printf("ENTER ON WHICH DAY DID IT HAPPEN: ");
         scanf("%d", &day);
@@ -176,120 +216,47 @@ void add_story(struct node** head){
         getchar();
     }while(year > 2023);
     
+    generate_filename(filename);
 
-    insert_node(head, name, day, month, year);
+    printf("%s", filename);
+    
+    insert_node(head, name, filename, day, month, year);
+    printf("\nafter insert\n");
 
     FILE *story;
     story = fopen(filename,"w");
     printf("\nENTER THE STORY (FOR NEW LINES TYPE @)\n");
     printf("\n");
     while(1){
-        i++;
         text[i] = getchar();
         if(text[i] == '\n' && text[i-1] != '!' && text[i-1] != '?' && text[i-1] != '.' && text[i-1] != ';'){
             fprintf(story, ".");
+            printf(".");
             break;
         }
         if(text[i] == '@'){
             fprintf(story, "\n");
+            printf("\n");
         }
         else{
             if(text[i] != ' ' && text[i-1] != '@'){
                 if(text[i] == ' '){
                     fprintf(story, " ");// it doesn't print spaces! 
+                    printf(" ");
                 }
                 fprintf(story, "%c", text[i]);
+                printf("%c", text[i]);
             }
         }
+        i++;
     }
-    fclose(story);
-    encrypt("1234", filename);
     printf("\n");
+    fclose(story);
 }
-
-void read_story(struct node* head, struct hashtable* table_for_dates, struct hashtable* table_for_titles){
-        int option;
-        char text[NUMBER_OF_SYMBOLS], name[HEADING_SUMBOLS];
-        char* filename;
-        FILE *read;
-        char secondValue[STRINGSIZE];
-        char sourceForHash[STRINGSIZE];
-        getchar();
-        while(1){
-            printf("CHOOSE TO ENTER DATE OR TITLE OF THE STORY\n");
-            printf("1) DATE\n");
-            printf("2) TITLE\n");
-            printf("Option: ");
-            scanf("%d", &option);
-            if(option == 1){
-                printf("\nENTER THE DATE OF THE STORY YOU WANT TO READ: ");
-                fgets(sourceForHash, STRINGSIZE, stdin);
-                sourceForHash[strlen(sourceForHash) - 1] = '\0';
-                
-                printSecondValues(table_for_dates, sourceForHash);
-
-                printf("Enter which one to open: ");
-                fgets(secondValue, STRINGSIZE, stdin);
-                secondValue[strlen(secondValue) - 1] = '\0';
-
-                filename = hashtable_contains(table_for_dates, sourceForHash, secondValue);
-            }
-            else if(option == 2){
-                printf("\nENTER THE DATE OF THE STORY YOU WANT TO READ: ");
-                fgets(sourceForHash, STRINGSIZE, stdin);
-                sourceForHash[strlen(sourceForHash) - 1] = '\0';
-                
-                printSecondValues(table_for_dates, sourceForHash);
-
-                printf("Enter which one to open: ");
-                fgets(secondValue, STRINGSIZE, stdin);
-                secondValue[strlen(secondValue) - 1] = '\0';
-
-                filename = hashtable_contains(table_for_dates, sourceForHash, secondValue);
-            }
-            else {
-                printf("There is no such option!\n");
-                break;
-            }
-
-            if(filename == NULL) {
-                printf("There is not such story!\n");
-                break;
-            }
-            
-            if((read = fopen(filename, "r")) != NULL){
-                printf("\n");
-                make_uppercase(name);
-                printf("    %s\n", name);
-                fclose(read);
-                /*while(fgets(text, NUMBER_OF_SYMBOLS, read)){
-                    printf("%s\n", text);
-                }*/
-                decrypt(filename, "1234");
-                printf("\n");
-                break;
-            }
-            else{
-                printf("ERROR WITH OPENING THE FILE\n");
-                break;
-            }
-        }
-
-        fclose(read);
-        
-        char stay[4];
-        printf("\nDO YOU WANT TO KEEP READING?: ");
-        scanf("%s", stay);
-        if(strcmp(stay,"yes") == 0){
-            print_menu(head);
-            read_story(head, table_for_dates, table_for_titles);
-        }
-}
-/*
 
 void read_story(struct node* head){
     if(head->name != NULL){
-        char text[NUMBER_OF_SYMBOLS], name[HEADING_SUMBOLS], filename[HEADING_SUMBOLS + 4];
+        char text[MAX_FILE_LINE_LENGTH], name[HEADING_SYMBOLS];
         FILE *read;
         getchar();
         while(1){
@@ -300,10 +267,8 @@ void read_story(struct node* head){
                 printf("\n");
                 while(temp != NULL){
                     printf("    %s\n", temp->name);
-                    strcpy(filename, temp->name);
-                    strcat(filename, ".txt");
-                    read = fopen(filename, "r");
-                    while(fgets(text, NUMBER_OF_SYMBOLS, read)){
+                    read = fopen(temp->filename, "r");
+                    while(fgets(text, MAX_FILE_LINE_LENGTH, read)){
                         printf("%s\n", text);
                     }
                     printf("\n");
@@ -313,14 +278,16 @@ void read_story(struct node* head){
                 break;
             }
             else{
-                strcpy(filename, name);
-                strcat(filename, ".txt");
-                if((read = fopen(filename, "r")) != NULL){
+                struct node* curr;
+                for (curr = head; head != NULL; head = head->next) {
+                    if (!strcmp(curr->name, name))break;
+                }
+                if((read = fopen(curr->filename, "r")) != NULL){
                     printf("\n");
                     make_uppercase(name);
                     printf("    %s\n", name);
 
-                    while(fgets(text, NUMBER_OF_SYMBOLS, read)){
+                    while(fgets(text, MAX_FILE_LINE_LENGTH, read)){
                         printf("%s\n", text);
                     }
                     printf("\n");
@@ -333,6 +300,8 @@ void read_story(struct node* head){
                 }
             }
         };
+    }
+/*
         char stay[4];
         printf("\nDO YOU WANT TO KEEP READING?: ");
         scanf("%s", stay);
@@ -341,26 +310,22 @@ void read_story(struct node* head){
             read_story(head);
         }
     }
-}
 */
-int main(){
-    int choice;
-    char text[SYMBOLS_IN_MENU];
-    struct node* head;//= _init_()
+}
 
+int main(){// NE PIPAJ
+    int choice;
+    char text[MAX_MENU_LINE_LENGTH];
+    struct node* head = NULL;//= _init_();
     FILE* menu;
     if((menu = fopen("menu.txt", "r")) != NULL){
-        while (fgets(text, SYMBOLS_IN_MENU, menu) != NULL)
+        while (fgets(text, MAX_MENU_LINE_LENGTH, menu) != NULL)
         {
+            if (*text == '\n') continue;
             getting_menu(&head, text);
         }
     }    
     fclose(menu);
-
-    struct  hashtable* table_for_titles = hashtable_init(10, 5);
-    struct  hashtable* table_for_dates = hashtable_init(10, 5);
-
-    fillingHashtables(head, table_for_dates, table_for_titles);
     
     printf("THIS IS PERSONAL DIARY PLEASE DON'T LOOK IN IT IF IT'S NOT YOURS!\n\n");
     do{
@@ -375,29 +340,26 @@ int main(){
 
         if(choice == 2){
             print_menu(head);
-            read_story(head, table_for_dates, table_for_titles);
+            read_story(head);
         }
 
     }while(choice != 3);
     menu = fopen("menu.txt", "w");
-    while(head != NULL){
+    struct node* temp1 = head,* temp2 = head;
+    while(temp1 != NULL){
         make_uppercase(head->name);
-        fprintf(menu, "%s @ %d.%d.%d\n", head->name, head->day, head->month, head->year);
-        head = head->next;
+        fprintf(menu, "%s @ %d.%d.%d @ %s\n", temp1->name, temp1->day, temp1->month, temp1->year, temp1->filename);
+        temp1 = temp1->next;
     }
     fclose(menu);
-    struct node* temp1 = head,* temp2 = head;
+    temp1 = head;
     while(temp1 != NULL){
         temp1 = temp1->next;
         free(temp2->name);
         free(temp2);
         temp2 = temp1;
     }
-    free(temp1);
-    free(temp2);
-    free(head);
+    //free(temp1);
+    //free(temp2);
     return 0;
 }
-
-
-//masiv ot stringove - char* string[] = {"","",...};
